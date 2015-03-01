716d1529ced85297950740bf46419f3dae584bfa
felixs8696 <felixs8696@berkeley.edu>
2015-03-01 15:40:34 -0800

    added add product button

--- fieldScript.js
+++ fieldScript.js
@@ -1,12 +1,34 @@
+var awareness = .01;
+var population = 100000;
+var assets = 100000000;
 function myFunction() {
-    // var prod = document.getElementById("product").value;
-    var perf = document.getElementById("performance").value;
-    var size = document.getElementById("size").value;
-    var MTBF = document.getElementById("MTBF").value;
-    var num = (perf * MTBF) / size;
-    var result = document.createElement("p");
-    var node = document.createTextNode("hi");
-    result.appendChild(node);
-    var element = document.getElementById("div1");
-	element.appendChild(result);
+    var price = document.getElementById("price").value;
+    var cost = document.getElementById("cost").value;
+    var marketing = document.getElementById("marketing").value;
+    var quantity = document.getElementById("quantity").value;
+    awareness = (marketing/1000000000) + awareness;
+    var sales = population * awareness * (price/(5*cost));
+    var profit = (sales * price) - (cost * quantity) - marketing;
+    assets += profit;
+}
+
+function addProduct() {
+    var myNewCode = '<div id= "product-item"><div class= "row"><div class= "col-lg-3"><h3> Product Name </h3></div></div><div class= "row"><div class= "col-lg-3">Price of Unit:<br>' +
+				 '	<input type="text" id="price" name="Product Name">' +
+					'</div>'+
+					'<div class= "col-lg-3">'+
+						'Cost per Unit:<br>'+
+						'<input type="text" id="cost" name="Performance">'+
+					'</div>'+
+					'<div class= "col-lg-3">'+
+						'Marketing Budget:<br>'+
+						'<input type="text" id = "marketing" name="Size">'+
+					'</div>'+
+					'<div class= "col-lg-3">'+
+						'Quantity Produced:<br>'+
+						'<input type="text" id="quantity" name="MTBF">'+
+					'</div>'+
+					'<br>'+
+			'</div>';
+    document.getElementById("temp-holder").innerHTML += myNewCode;
 }
